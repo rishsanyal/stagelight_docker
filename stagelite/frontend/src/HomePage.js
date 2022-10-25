4169cefe10ed3ddde10486da6da47481cd2d6d1e
weirdboy16 <rishabh106@gmail.com>
2022-10-25 03:14:18 -0700

    Changing workstations

---
+++ stagelite/frontend/src/HomePage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import {Heading, Subhead } from 'rebass'
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div>HomePage</div>
+    )
+  }
+}
